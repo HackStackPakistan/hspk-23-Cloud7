463862018c4b115505f7ae353a98f5a8fddf7e1e
Talha Faisal <talha.faisal2000@gmail.com>
2023-09-30 23:15:44 +0500

    Add Firebase Authentication on Login Page

--- frontend-portal/src/environments/environment.ts
+++ frontend-portal/src/environments/environment.ts
@@ -1,13 +1,10 @@
 export const environment = {
   firebase: {
-    projectId: 'automark-ai',
-    appId: '1:65559924006:web:6fd215982f20c8c89f85e7',
-    databaseURL: 'https://automark-ai-default-rtdb.asia-southeast1.firebasedatabase.app',
-    storageBucket: 'automark-ai.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyAXVlMk1O1tbo4d3tyUZZ30H_7Rq6LyNik',
-    authDomain: 'automark-ai.firebaseapp.com',
-    messagingSenderId: '65559924006',
-    measurementId: 'G-Y2V8V3PE5E',
+    apiKey: "AIzaSyADjcTTonOdddLjU1QLL_4X3COcJgfvK0Q",
+    authDomain: "automark-9bcb3.firebaseapp.com",
+    projectId: "automark-9bcb3",
+    storageBucket: "automark-9bcb3.appspot.com",
+    messagingSenderId: "380911672161",
+    appId: "1:380911672161:web:82ffa8e8467443d470c320"
   },
 };
